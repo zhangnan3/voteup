82014b1f09d6b709bbfa2be52a15e8f746ac4842
zhangnan3 <1437613243@qq.com>
2017-10-13 15:06:24 +0000

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './components/home.vue'
+export default {
+  name: 'app',
+  components:{Home}
+}
+</script>
+
+<style>
+#app {
+   width: 1200px;
+   margin: 0 auto;
+   background-color: lightblue;
+   height: 100vh;
+   padding-top: 50px;
+}
+*{
+  margin: 0;
+}
+</style>
